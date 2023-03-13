2c306ec2e47a04eeac4b0661e2c14a8228f0594a
alaa77alghamdi <98323604+alaa77alghamdi@users.noreply.github.com>
2023-03-13 10:39:52 +0300

    change in the route

--- src/Components/Routes.tsx
+++ src/Components/Routes.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import { Route, Routes as Routess } from 'react-router-dom'
-import AboutUs from './AboutUs'
-import Auctions from './Auctions'
-import AuctionsDetails from './AuctionsDetails'
-import ContactUs from './ContactUs'
-import Home from './Home'
-import Login from './Login'
+import React from "react";
+import { Route, Routes as Routess } from "react-router-dom";
+import AboutUs from "./AboutUs";
+import Auctions from "./Auctions";
+import AuctionsDetails from "./AuctionsDetails";
+import CardHome from "./CardsHome/CardsHome";
+import ContactUs from "./ContactUs";
+import Home from "./Home";
+import Login from "./Login";
 
 function Routes() {
   return (
     <Routess>
-          <Route  path='/' element={<Home />}></Route>
-          <Route  path='/about' element={<AboutUs />}></Route>
-          <Route  path='/auctions' element={<Auctions />}></Route>
-          <Route  path='/auctionDetails/:id' element={<AuctionsDetails />}></Route>
-          <Route  path='/contact' element={<ContactUs />}></Route>
-          <Route  path='/login' element={<Login />}></Route>
-        </Routess>
-  )
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<AboutUs />}></Route>
+      <Route path="/auctions" element={<Auctions />}></Route>
+      <Route path="/auctionDetails/:id" element={<AuctionsDetails />}></Route>
+      <Route path="/contact" element={<ContactUs />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+    </Routess>
+  );
 }
 
-export default Routes
+export default Routes;
