382589346fcae45c766cb46cfcf2deeba96b363e
samah <samahnagdi46@gmail.com>
2023-03-14 11:06:32 +0300

    Add Auctions

---
+++ src/controllers/auctions.controller.ts
@@ -0,0 +1,30 @@
+
+import {Request, Response} from 'express';
+import { prisma } from "../config/db";
+
+
+
+export const addAuction=async (req:Request,res:Response)=>{
+
+    let Auctions=await prisma.auction.create({
+    
+      data:{
+        userId:res.locals.id,
+        title:req.body.title,
+        started_date:req.body.started_date,
+        end_date:req.body.end_date,
+        auction_type:req.body.auction_type,
+        auction_price:req.body.auction_price,
+        auction_min_price:req.body.auction_min_price,
+        auction_max_price:req.body.auction_max_price,
+        is_online:req.body.is_online,
+        auction_images:req.body.auction_images
+    
+      }
+    
+    })
+    res.status(200).json({
+        status: 'success',
+      });
+    
+    }
