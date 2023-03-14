2aa6d6acb60331d8fe755c917fd794394e78f383
rwishd17 <rwishd17@gmail.com>
2023-03-14 17:35:20 +0300

    Resolve Some Errors

--- src/controllers/auctions.controller.ts
+++ src/controllers/auctions.controller.ts
@@ -1,44 +1,65 @@
-import {Request, Response} from 'express';
-import {prisma} from '../config/db';
+import { Auction } from '@prisma/client';
+import { Request, Response } from 'express';
+import { prisma } from '../config/db';
 
 
 
 
-export const addAuction=async (req:Request,res:Response)=>{
+export const addAuction = async (req: Request, res: Response) => {
 
-  console.log(res.locals.id);
-  
+  const c_aucation = req.body as Auction
 
-let Auctions=await prisma.auction.create({
+  try {
 
-  data:{
-    userId:res.locals.id,
-    title:req.body.title,
-    started_date:req.body.started_date,
-    end_date:req.body.end_date,
-    is_accepted:req.body.is_accepted,
-    auction_type:req.body.auction_type,
-    auction_price:req.body.auction_price,
-    auction_min_price:req.body.auction_min_price,
-    auction_max_price:req.body.auction_max_price,
-    is_online:req.body.is_online,
-    auction_status:req.body.auction_status
+    const auction =  await prisma.auction.create({
+      data: {
+        title: c_aucation.title,
+        userId: res.locals.user.id,
+        auction_type: c_aucation.auction_type,
+        started_date: c_aucation.started_date,
+        end_date: c_aucation.end_date,
+        auction_status: c_aucation.auction_status,
+        is_accepted: c_aucation.is_accepted,
+        auction_price: c_aucation.auction_price,
+        auction_min_price: c_aucation.auction_min_price,
+        auction_max_price: c_aucation.auction_max_price,
+        is_online: c_aucation.is_online
+      }
+    })
 
-  }  
+    if(auction) {
 
-})
-res.status(200).json({
-    status: 'success',
-  });
+      return res.status(200).json({
+        message: "auction created successfully!",
+        status: res.statusCode,
+      });
+
+    }
+
+    throw("there was an error, try again!");
+
+
+  } catch(err) {
+
+    res.json(err)
+  }
+
+  console.log(res.locals.user.id);
+  console.log('aucation', c_aucation)
+
+  console.log("---------------------------------------------");
+
+  // console.log('Auctions', Auctions)
 
 }
 
-export const GetallAuctions= async (req: Request, res: Response) => {
+export const GetallAuctions = async (req: Request, res: Response) => {
   try {
     let Auctoins = await prisma.auction.findMany();
 
     res.json({ "Auctoins": Auctoins });
   } catch (err) {
     console.log(err);
+    res.status(500).json(err)
   }
 };
