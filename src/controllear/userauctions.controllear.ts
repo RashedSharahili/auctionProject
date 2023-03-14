6e366ded083a21b054cad4b924aae1aa23a63a6b
alaa77alghamdi <98323604+alaa77alghamdi@users.noreply.github.com>
2023-03-14 19:20:51 +0300

    change in the prisma

--- src/controllear/userauctions.controllear.ts
+++ src/controllear/userauctions.controllear.ts
@@ -1,24 +1,24 @@
 
 
-import {Request, Response} from 'express';
-import {prisma} from '../config/db';
+// import {Request, Response} from 'express';
+// import {prisma} from '../config/db';
 
 
 
-export const addAuction=async (req:Request,res:Response)=>{
+// export const addAuction=async (req:Request,res:Response)=>{
 
-let Auctions=await prisma.userAuction.create({
-    where:{
-        userId: res.locals.user.id
-    },
-  data:{
-    auction_price:req.body.auction_price
+// let Auctions=await prisma.userAuction.create({
+//     where:{
+//         userId: res.locals.user.id
+//     },
+//   data:{
+//     auction_price:req.body.auction_price
 
-  }  
+//   }  
 
-})
-res.status(200).json({
-    status: 'success',
-  });
+// })
+// res.status(200).json({
+//     status: 'success',
+//   });
 
-}                       
+// }                       
