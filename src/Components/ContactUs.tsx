60ef6c1581831722344bf9e53771aa347a471c03
alaa77alghamdi <98323604+alaa77alghamdi@users.noreply.github.com>
2023-03-15 14:59:45 +0300

    some change in the contact

--- src/Components/ContactUs.tsx
+++ src/Components/ContactUs.tsx
@@ -28,7 +28,7 @@ import { BsGithub, BsDiscord, BsPerson } from 'react-icons/bs';
 
 export default function contact() {
   return (
-    <Container bg="#9DC4FB" maxW="full" mt={0} centerContent overflow="hidden">
+    <Container bg="" maxW="full" mt={0} centerContent overflow="hidden">
       <Flex>
         <Box
           bg="#02054B"
@@ -40,9 +40,9 @@ export default function contact() {
             <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
               <WrapItem>
                 <Box>
-                  <Heading>Contact</Heading>
+                  <Heading>تواصل معنا </Heading>
                   <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
-                    Fill up the form below to contact
+                 
                   </Text>
                   <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
                     <VStack pl={0} spacing={3} alignItems="flex-start">
