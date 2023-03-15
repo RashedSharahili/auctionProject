a0b23ffb5f2d3dddb61b1885b10a519f9e289887
alaa77alghamdi <98323604+alaa77alghamdi@users.noreply.github.com>
2023-03-15 15:14:52 +0300

    -change the user info

--- src/schema.zod/aucations.zod.ts
+++ src/schema.zod/aucations.zod.ts
@@ -4,47 +4,47 @@ export const createAuctionSchema = z.object({
 
     body: z.object({
         auction_type: z.string({
-            required_error: "auction_type is required",
-            invalid_type_error: "auction_type must be number"
+            required_error: "اختر نوع المزاد!",
+            invalid_type_error: ""
         }),
         
         title: z.string({
-            required_error: "Title is required",
-            invalid_type_error: "Title must be string"
+            required_error: "اكتب عنوان المزاد",
+            invalid_type_error: "يجب ان يكون حروف"
         }),
 
        started_date: z.string({
-            required_error: "started_date is requird!",
-            invalid_type_error: "started_date must be dateTime"
+            required_error: "اختر يوم!",
+            invalid_type_error: "يجب ان يكون صحيح"
         }),
 
 
         end_date: z.string({
-            required_error: "end_date is requird!",
-            invalid_type_error: "started_date must be dateTime"
+            required_error: "اختر يوم !",
+            invalid_type_error: "يجيب ان يكون صحيح"
         }),
         auction_status: z.boolean({
-            invalid_type_error: "auction_status must be boolean"
+            invalid_type_error: "اختر الحاله!"
         }),
 
         auction_price: z.number({
-            required_error: "auction_price is requird!",
-            invalid_type_error: "auction_price must be int"
+            required_error: "اختر سعر المزاد!",
+            invalid_type_error: "يجب ان يكون ارقام"
         }),
         auction_min_price: z.number({
-            required_error: "auction_min_price is requird!",
-            invalid_type_error: "auction_min_price must be int"
+            required_error: "اختر حد ادنى!",
+            invalid_type_error: "يجب ان يكون ارقام"
         }),
 
 
         auction_max_price: z.number({
-            required_error: "auction_max_price is requird!",
-            invalid_type_error: "auction_max_price must be int"
+            required_error: "اختر حد اعلى",
+            invalid_type_error: "يجب ان يكون ارقام"
         }),
 
 
         is_online: z.boolean({
-            invalid_type_error: "is_online must be boolean"
+            invalid_type_error: "لا يمكن ان يكون المزاد عن بعد!"
         }),
         
 
