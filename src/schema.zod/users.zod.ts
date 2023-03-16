7bee0cfc956103cce4c2cb3028181a27c25fbb9f
alaa77alghamdi <98323604+alaa77alghamdi@users.noreply.github.com>
2023-03-16 15:36:19 +0300

    change in the zod

--- src/schema.zod/users.zod.ts
+++ src/schema.zod/users.zod.ts
@@ -13,14 +13,14 @@ export const createUserSchema = z.object({
             required_error: "كلمة المرور مطلوبة!"
         }).min(6, "يجب ان تحتوي كلمة المرور على ٦ خانات"),
 
-        phone_number:z.string({
-            required_error:"رقم الجوال مطلوب",
-            invalid_type_error:"يجب ان يكون رقم"
-        }),
-        birthDate:z.string({
-            required_error:"تاريخ الميلاد مطلوب"
+        // phone_number:z.string({
+        //     required_error:"رقم الجوال مطلوب",
+        //     invalid_type_error:"يجب ان يكون رقم"
+        // }),
+        // birthDate:z.string({
+        //     required_error:"تاريخ الميلاد مطلوب"
            
-        })
+        // })
 
     })
 });
